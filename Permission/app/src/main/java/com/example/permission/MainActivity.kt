1d56635c43df7ee4329d71a4f45811fa0fb5db66
realhsb <realhsb@gmail.com>
2022-03-17 11:57:04 +0900

    3rd edition

---
+++ Permission/app/src/main/java/com/example/permission/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.permission
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun checkPermission(){
+        /*
+        카메라 권한의 승인 상태를 먼저 확인한 다음 결과
+         */
+
+    }
+}
